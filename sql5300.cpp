90db2237ed0a909ca3afd3cd08a57518610ee9c0
Ben <gruherb@seattleu.edu>
2021-04-07 15:37:33 -0700

    Added DB environment. Started parseTree unparsing. Detecting statement type working

--- sql5300.cpp
+++ sql5300.cpp
@@ -3,6 +3,12 @@
 #include "db_cxx.h"
 #include "SQLParser.h"
 
+
+const char *HOME = "cpsc5300/data";
+const char *EXAMPLE = "example.db";
+const unsigned int BLOCK_SZ = 4096;
+
+
 /**
  * Converts the hyrise parse tree back into a SQL string
  * @param parseTree	hyrise parse tree pointer
@@ -10,12 +16,37 @@
  */
 std::string execute(hsql::SQLParserResult* parseTree) {
 	// FIXME - implement execute
-	return "Execute return value here- query was valid";
+	for(size_t i = 0; i < parseTree->size(); i++) {
+		switch (parseTree->getStatement(i)->type()) {
+		case hsql::kStmtSelect:
+			std::cout << "This was a select statement" << std::endl;
+      			break;
+		case hsql::kStmtCreate:
+			std::cout << "This was a create statement" << std::endl;
+		}
+		// std::cout << parseTree->getStatement(i) << std::endl;
+	}
+	return "VALID";
 }
 
 int main(void) {
-	// FIXME - insert Berkeley DB statements here
+	// Berkeley DB statements here
+	const char *home = std::getenv("HOME");
+	std::string envdir = std::string(home) + "/" + HOME;
 	
+	DbEnv env(0U);
+	env.set_message_stream(&std::cout);
+	env.set_error_stream(&std::cerr);
+	env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
+
+	Db db(&env, 0);
+	db.set_message_stream(env.get_message_stream());
+	db.set_error_stream(env.get_error_stream());
+	db.set_re_len(BLOCK_SZ); // Set record length to 4K
+	db.open(NULL, EXAMPLE, NULL, DB_RECNO, DB_CREATE | DB_TRUNCATE, 0644); // Erases anything already there
+
+
+
 	std::cout << "quit to end" << std::endl;
 	std::string query;
 	while(true) {
@@ -37,8 +68,10 @@ int main(void) {
 		}
 		else {
 			// print error message
-			std::cout << "Invalid SQL Statement" << std::endl;
+			std::cout << "Invalid SQL: " << query << std::endl;
 		}
+		
+		delete parseTree;
 
 		std::cout << query << std::endl;
 	}
