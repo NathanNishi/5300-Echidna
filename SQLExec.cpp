fe3b77b3da16ef328a07478a2305be027d14b1b8
tuanphan23 <anhtuan.ph235@gmail.com>
2021-04-30 23:53:19 -0700

    Implemented create and drop in SQLExec.cpp

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -43,13 +43,27 @@ ostream &operator<<(ostream &out, const QueryResult &qres) {
 }
 
 QueryResult::~QueryResult() {
-    // FIXME
+    if (column_names != nullptr) {
+        delete column_names;
+    }
+    if (column_attributes != nullptr) {
+        delete column_attributes;
+    }
+    if (rows != nullptr) {
+        for (auto row : *rows) {
+            delete row;
+        }
+        delete rows;
+    }
 }
 
 
 QueryResult *SQLExec::execute(const SQLStatement *statement) {
-    // FIXME: initialize _tables table, if not yet present
-
+    //initialize _tables table, if not yet present
+    if (SQLExec::tables == nullptr) {
+        SQLExec::tables = new Tables();
+    }
+    
     try {
         switch (statement->type()) {
             case kStmtCreate:
@@ -68,16 +82,106 @@ QueryResult *SQLExec::execute(const SQLStatement *statement) {
 
 void
 SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute) {
-    throw SQLExecError("not implemented");  // FIXME
+    column_name = col->name;
+    switch (col->type) {
+    case ColumnDefinition::TEXT:
+        column_attribute.set_data_type(ColumnAttribute::TEXT);
+        break;
+    case ColumnDefinition::INT:
+        column_attribute.set_data_type(ColumnAttribute::INT);
+        break;
+    default:
+        throw SQLExecError("Data type is not supported");
+    }
 }
 
 QueryResult *SQLExec::create(const CreateStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+    if (statement->type != CreateStatement::kTable) {
+        return new QueryResult("Unrecognized CREATE type");
+    }
+
+    // update table schema
+    Identifier table_name = statement->tableName;
+    ValueDict row;
+    row["table_name"] = table_name;
+    Handle table_handle = SQLExec::tables->insert(&row);
+
+    // get column schema
+    Identifier col_name;
+    ColumnNames col_names;
+    ColumnAttribute col_attribute;
+    ColumnAttributes col_attributes;
+    for (ColumnDefinition* col : *statement->columns) {
+        column_definition(col, col_name, col_attribute);
+        col_names.push_back(col_name);
+        col_attributes.push_back(col_attribute);
+    }
+
+    // update column schema
+    try {
+        Handles col_handles;
+        DbRelation& columns = SQLExec::tables->get_table(Columns::TABLE_NAME);
+
+        try {
+            for (unsigned int i = 0; i < col_names.size(); i++) {
+                row["column_name"] = col_names[i];
+                row["data_type"] = Value(
+                        col_attributes[i].get_data_type()
+                                == ColumnAttribute::INT ? "INT" : "TEXT");
+                col_handles.push_back(columns.insert(&row));
+            }
+            DbRelation& table = SQLExec::tables->get_table(table_name);
+            if (statement->ifNotExists) {
+                table.create_if_not_exists();
+            } else {
+                table.create();
+            }
+        } catch (exception& e) {
+            try {
+                for (unsigned int i = 0; i < col_handles.size(); i++) {
+                    columns.del(col_handles.at(i));
+                }
+            } catch (...) {
+            }
+            throw;
+        }
+    } catch (exception& e) {
+        try {
+            SQLExec::tables->del(table_handle);
+        } catch (...) {
+        }
+        throw;
+    }
+    return new QueryResult("Created " + table_name);
 }
 
 // DROP ...
 QueryResult *SQLExec::drop(const DropStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+    if (statement->type != DropStatement::kTable) {
+        return new QueryResult("Unrecognized DROP type");
+    }
+
+    Identifier table_name = statement->name;
+
+    if (table_name == Tables::TABLE_NAME || table_name == Columns::TABLE_NAME) {
+        throw SQLExecError("Cannot drop a schema table!");
+    }
+
+    DbRelation& drop_table = SQLExec::tables->get_table(table_name);
+
+    ValueDict where;
+    where["table_name"] = Value(table_name);
+    DbRelation& column = SQLExec::tables->get_table(Columns::TABLE_NAME);
+    Handles* col_handle = column.select(&where);
+    for (unsigned int i = 0; i < col_handle->size(); i++) {
+        column.del(col_handle->at(i));
+    }
+
+    delete col_handle;
+    drop_table.drop();
+
+    SQLExec::tables->del(*SQLExec::tables->select(&where)->begin());
+    return new QueryResult("dropped " + table_name);
 }
 
 QueryResult *SQLExec::show(const ShowStatement *statement) {
